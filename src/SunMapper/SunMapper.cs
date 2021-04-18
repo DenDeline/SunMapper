505175d9480a6d6bba8597679d2642e27a64b615
Rostislav <statko.rostik@gmail.com>
2021-04-18 20:12:39 +0300

    Delete .csproj.user files

--- src/SunMapper/SunMapper.cs
+++ src/SunMapper/SunMapper.cs
@@ -1,4 +1,10 @@
-﻿namespace SunMapper
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunMapper
 {
     public class SunMapper
     {
