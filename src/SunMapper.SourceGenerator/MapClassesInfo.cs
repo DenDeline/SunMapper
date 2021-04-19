b79eb22cae3af3791f1344a5b6a104d5269b9c48
Rostislav <statko.rostik@gmail.com>
2021-04-20 00:34:38 +0300

    Implement simple fluent API for mapper

---
+++ src/SunMapper.SourceGenerator/MapClassesInfo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SunMapper.SourceGenerator
+{
+    public record MapClassesInfo
+    {
+        public ITypeSymbol SourceClassType { get; }
+        public ITypeSymbol DestinationClassType { get; }
+
+        public MapClassesInfo(ITypeSymbol sourceClassType, ITypeSymbol destinationClassType)
+        {
+            SourceClassType = sourceClassType;
+            DestinationClassType = destinationClassType;
+        }
+    }
+}
---
+++ src/SunMapper.SourceGenerator/MapperInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunMapper.SourceGenerator
+{
+    public class MapperInfo
+    {
+        public ITypeSymbol Destination { get; set; }
+        public ITypeSymbol Source { get; set; }
+
+        public MapperInfo(ITypeSymbol source, ITypeSymbol destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+    }
+}
