0c04e77e565ac34f691b550805dc2288b2b125d6
Rostislav <statko.rostik@gmail.com>
2021-04-21 00:59:24 +0300

    Implementation simple properties mapping

--- test/SunMapper.UnitTests/UnitTest1.cs
+++ test/SunMapper.UnitTests/UnitTest1.cs
@@ -1,29 +1,44 @@
 
+using System;
 using SunMapper.Common.Attributes;
 using SunMapper.Generated.Extensions;
 using Xunit;
 
 namespace SunMapper.UnitTests
 {
+    [MapTo(typeof(UserGetDto))]
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    [MapTo(typeof(User))]
+    public class UserGetDto
+    {
+        public string Name { get; set; }
+    }
+    
     public class UnitTest1
     {
-        [MapTo(typeof(UserGetDto))]
-        public class User
-        {
-
-        }
-
-        [MapTo(typeof(User))]
-        public class UserGetDto
-        {
-
-        }
-        
         [Fact]
         public void Test1()
         {
             var user = new User();
-            Assert.True(user.TryMapTo(out UserGetDto dest));
+
+            Assert.True(user.TryMapTo(out UserGetDto destination));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var user = new User
+            {
+                Name = "User1"
+            };
+
+            Assert.True(user.TryMapTo(out UserGetDto destination));
+            Assert.Equal("User1", destination.Name);
         }
     }
 }
