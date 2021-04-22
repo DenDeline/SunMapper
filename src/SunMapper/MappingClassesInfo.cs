1eb152c5e9005208935e4e46fcd5dbb93ac2a9cf
Rostislav <statko.rostik@gmail.com>
2021-04-22 22:33:08 +0300

    Refactor

---
+++ src/SunMapper/MappingClassesInfo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SunMapper
+{
+    public class MappingClassesInfo
+    {
+        public INamedTypeSymbol Destination { get; }
+        public INamedTypeSymbol Source { get; }
+
+        public MappingClassesInfo(INamedTypeSymbol source, INamedTypeSymbol destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+    }
+}
