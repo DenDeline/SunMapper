b8c55c776c7a0e7202ee4f4e566d42be240f1245
Rostislav <statko.rostik@gmail.com>
2021-04-22 21:42:03 +0300

    Add docs for MapToAttribute

--- src/SunMapper.Common/Attributes/MapToAttribute.cs
+++ src/SunMapper.Common/Attributes/MapToAttribute.cs
@@ -2,11 +2,19 @@
 
 namespace SunMapper.Common.Attributes
 {
+    /// <summary>
+    /// Allow SunMapper to find classes from you are mapping 
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
     public sealed class MapToAttribute : Attribute
     {
         public Type Desination { get; }
 
+        /// <summary>
+        /// Allow SunMapper to find classes from you are mapping 
+        /// </summary>
+        /// <param name="destination">Destination mapping class type</param>
+        /// <remarks> SunMapper will generate TryMapTo extension method for this class </remarks>
         public MapToAttribute(Type destination)
         {
             Desination = destination;
